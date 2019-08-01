a7d9940552d5489e5b3788171b0c2a8b9d2caca9
Lorena Pantano <lorena.pantano@gmail.com>
2019-08-01 10:10:17 -0400

    move legacy files to folder

--- r_code/00_libsize.R
+++ r_code/00_libsize.R
@@ -1,31 +1,14 @@
-library(tidyverse)
-library(ggplot2)
-library(pheatmap)
-theme_set(
-    theme_light(base_size = 11L))
-theme_update(
-    legend.justification = "center",
-    legend.position = "bottom")
-
-
-equimolar_razer3 %>% 
-    group_by(sample, protocol) %>% 
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
     summarise(n=n(),size=sum(value)) %>% 
     mutate(ytext = size + size*0.1,
            text = paste0(round(n/1000, digits = 0), "K")) %>% 
-    ggplot(aes(sample, size, fill = protocol)) +
+    ggplot(aes(paste(study, short), size, fill = protocol)) +
     geom_bar(stat = "identity") +
     ylab("reads") +
-    geom_text(aes(sample, ytext, label = text), size = 3) +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
-    ggsave("results/00_library_size/00_libsize_diversity_bar.pdf", width = 9)
+    geom_text(aes(paste(study, short), ytext, label = text), size = 3) +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) 
 
-equimolar_razer3 %>% 
-    group_by(sample, protocol) %>% 
-    summarise(n=n(),size=sum(value)) %>% 
-    mutate(ytext = size + size*0.1,
-           text = paste0(round(n/1000, digits = 0), "K")) %>% 
-    ggplot(aes(size, n, color = protocol)) +
-    scale_x_log10() +
-    geom_point(size=3) +
-    ggsave("results/00_library_size/00_libsize_diversity_scatter.pdf")
+
+#    ggsave("results/00_library_size/00_libsize_diversity_bar.pdf", width = 9)
--- r_code/04_other_data.R
+++ r_code/04_other_data.R
@@ -1,214 +1,140 @@
 library(tidyverse)
 library(ggplot2)
-library(pheatmap)
-theme_set(
-    theme_light(base_size = 11L))
+library(ggthemes)
 theme_update(
     legend.justification = "center",
     legend.position = "bottom")
+synthetic = readRDS("data/synthetic_2019_mirgff1.2.rds")
 
 prepare =  . %>% filter(ref_is_1 == 1) %>%
-    dplyr::count(protocol, sample, short, pct_cat, iso) %>% 
-    group_by(protocol) %>% 
-    mutate(sample2 = paste0(protocol, "_", 1:length(unique(sample)))) %>% 
-    group_by(sample2, protocol) %>% 
-    mutate(pct_total = n/sum(n)*100,
-           iso = gsub("snp", "snv", iso),
-           iso = ifelse(grepl("snv ", iso), "snv + other", iso),
-           iso = ifelse(grepl("add3p ", iso), "add3p + other", iso)) %>% 
-    filter(iso  %in% c("shift5p", "shift3p", "snv", "reference"))
+    dplyr::count(short, pct_cat, iso, protocol, study, libsize) %>% 
+    group_by(short, protocol, study, libsize) %>% 
+    mutate(pct_total = n/sum(n)*100)
 
-bind_rows(
-    equimolar_razer3 %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "tewari synthetic"),
-    
-    vandijk  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "vandijk"), 
-    
-    custom  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "tewari_custom"),
-    
-    narrykim  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "narrykim_synthetic"),
-) %>% 
-    ggplot(aes(x = protocol, y = pct_total, color = pct_cat)) +
-    geom_boxplot(outlier.color = NA) +
-    facet_grid(iso~tool, scales = "free") +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
-          strip.text = element_text(size = 14, color = "black")) +
-    ylab("% of sequences") +
-    scale_color_manual("IMPORTANCE",
-                       values = RColorBrewer::brewer.pal(7, "Dark2")[3:7]) +
-    ggsave("results/04_other_data/04_other_data_pct_g5.pdf", height = 7)
-
-
-bind_rows(
-    equimolar_razer3 %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "tewari"),
-    
-    vandijk  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "vandijk"), 
-    
-    custom  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "tewari_custom"),
-    
-    dsrg  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        ungroup() %>% 
-        mutate(tool = "dsrg",
-               protocol = ifelse(protocol == "ilmn", "tru", protocol)),
-    
-    narrykim  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "narrykim_synthetic"),
-
-    plasma  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>%
-        ungroup() %>% 
-        mutate(tool = "human plasma",
-               protocol = ifelse(protocol == "TrueSeq", "tru", "neb"))
-) %>% 
-    ggplot(aes(x = protocol, y = pct_total, color = pct_cat)) +
-    geom_boxplot(outlier.color = NA) +
-    facet_grid(iso~tool, scales = "free") +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
-          strip.text = element_text(size = 14, color = "black")) +
-    ylab("PCT") +
-    scale_color_manual("IMPORTANCE",
-                       values = RColorBrewer::brewer.pal(7, "Dark2")[3:7]) +
-    ggsave("results/04_other_data/04_other_alldata_pct_g5.pdf", height = 9)
-
-# barplots
-bind_rows(
-    equimolar_razer3 %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "tewari"),
-    
-    vandijk  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "vandijk"), 
-    
-    custom  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "tewari_custom"),
-    
-    dsrg  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        ungroup() %>% 
-        mutate(tool = "dsrg",
-               protocol = ifelse(protocol == "ilmn", "tru", protocol)),
-    
-    narrykim  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>% 
-        mutate(tool = "narrykim_synthetic"),
-    
-    plasma  %>% 
-        filter(pct > 5) %>% 
-        prepare() %>%
-        ungroup() %>% 
-        mutate(tool = "human plasma",
-               protocol = ifelse(protocol == "TrueSeq", "tru", "neb"))
-) %>% 
-    ungroup() %>% 
-    mutate(index = as.numeric(as.factor(sample)),
-           clean_name = paste0(tool, "_", protocol, "_", index)) %>% 
-    ggplot(aes(x = clean_name, y = pct_total, fill = pct_cat)) +
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
+    mutate(libsize=log10(sum(value)),
+           iso=relevel(as.factor(iso), "reference")) %>% 
+    filter(total>500) %>% 
+    prepare() %>%
+    ggplot(aes(x = paste(study, short),
+               fill = pct_cat, y = pct_total,
+               alpha=libsize>5)) +
     geom_bar(stat = "identity") +
-    theme(axis.text.x = element_text(size=7, angle = 90, hjust = 1, vjust = 0.5),
-          strip.text = element_text(size = 14, color = "black")) +
-    ylab("% of sequences") +
-    ylim(0,100) +
-    scale_fill_manual("IMPORTANCE",
-                       values = RColorBrewer::brewer.pal(7, "Dark2")[4:7]) +
-    facet_wrap(~iso, nrow = 4) +
-    ggsave("results/04_other_data/04_other_alldata_pct_g5_bar.pdf", height = 9, width = 12)
-
-bind_rows(narrykim %>% mutate(short=paste("nk_", short)),
-          narrykim_human %>% mutate(short=paste("nkh_", short)))%>%
-    filter(iso  %in% c("shift5p", "shift3p", "snp")) %>%
-    ggplot(aes(y=short,x=pct)) +
-    ggridges::geom_density_ridges(color=NA) +
-    geom_vline(xintercept = 10) +
-    facet_wrap(~iso, nrow = 1,  scales = "free_x") +
-    ggsave("results/04_other_data/04_other_nk_pct_g10_density.pdf", height = 9)
-
-bind_rows(plasma %>% mutate(short=paste("twh_", short)),
-          equimolar_razer3 %>% mutate(short=paste("tw_", short)))%>%
-    filter(iso  %in% c("shift5p", "shift3p", "snp")) %>%
-    ggplot(aes(y=short,x=pct)) +
-    ggridges::geom_density_ridges(color=NA) +
-    geom_vline(xintercept = 10) +
-    facet_wrap(~iso, nrow = 1,  scales = "free_x") +
-    ggsave("results/04_other_data/04_other_tw_pct_g10_density.pdf", height = 9)
+    facet_wrap(~iso, ncol = 1, scales = "free_y") +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
+    scale_alpha_discrete(range = c(0.3,1)) +
+    ggtitle("isomiRs importance by type") +
+    ylab("% of sequences") 
 
 
+prepare =  . %>% filter(ref_is_1 == 1) %>%
+    dplyr::count(short, pct_cat, iso, protocol, study, libsize, iso_n) %>% 
+    group_by(short, protocol, study, libsize) %>% 
+    mutate(pct_total = n/sum(n)*100)
+    
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
+    mutate(libsize=log10(sum(value))) %>% 
+    filter(total>500, iso=="iso_3p") %>% 
+    prepare() %>%
+    ggplot(aes(x = paste(study, short),
+               fill = pct_cat, y = n,
+               alpha=libsize>5)) +
+    geom_bar(stat = "identity") +
+    facet_wrap(~iso_n, ncol = 1, scales = "free_y") +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
+    scale_alpha_discrete(range = c(0.3,1)) +
+    ggtitle("isomiRs importance by size") +
+    ylab("% of sequences") 
 
-bind_rows(
-    equimolar_razer3 %>% 
-        filter(pct > 1) %>% 
-        prepare() %>% 
-        filter(iso  %in% c("shift5p", "shift3p", "snp")) %>% 
-        mutate(tool = "tewari"),
-    plasma  %>% 
-        filter(pct > 1) %>% 
-        prepare() %>%
-        ungroup() %>% 
-        filter(iso  %in% c("shift5p", "shift3p", "snp")) %>% 
-        mutate(tool = "human plasma",
-               protocol = ifelse(protocol == "TrueSeq", "tru", "neb"))
-) %>% 
-    ggplot(aes(x = protocol, y = pct_total, color = pct_cat)) +
-    geom_boxplot(outlier.color = NA) +
-    facet_grid(iso~tool, scales = "free") +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
-          strip.text = element_text(size = 14, color = "black")) +
-    ylab("PCT") +
-    scale_color_manual("IMPORTANCE",
-                       values = RColorBrewer::brewer.pal(7, "Dark2")[3:7]) +
-    ggsave("results/04_other_data/04_other_alltewari_pct_g1.pdf", height = 9)
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
+    mutate(libsize=log10(sum(value))) %>% 
+    filter(total>500, iso=="iso_5p") %>% 
+    prepare() %>%
+    ggplot(aes(x = paste(study, short),
+               fill = pct_cat, y = n,
+               alpha=libsize>5)) +
+    geom_bar(stat = "identity") +
+    facet_wrap(~iso_n, ncol = 1, scales = "free_y") +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
+    scale_alpha_discrete(range = c(0.3,1)) +
+    ggtitle("isomiRs importance by size") +
+    ylab("% of sequences") 
 
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
+    mutate(libsize=log10(sum(value))) %>% 
+    filter(total>500, iso=="iso_snp") %>% 
+    prepare() %>%
+    ggplot(aes(x = paste(study, short),
+               fill = pct_cat, y = n,
+               alpha=libsize>5)) +
+    geom_bar(stat = "identity") +
+    facet_wrap(~iso_n, ncol = 1, scales = "free_y") +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
+    scale_alpha_discrete(range = c(0.3,1)) +
+    ggtitle("isomiRs importance by size") +
+    ylab("% of sequences") 
+#    ggsave("results/04_other_data/04_other_data_pct_g5.pdf", height = 7)
 
-bind_rows(
-    equimolar_razer3 %>% 
-        filter(pct > 1) %>% 
-        prepare() %>% 
-        filter(iso  %in% c("shift5p", "shift3p", "snp")) %>% 
-        mutate(tool = "tewari"),
-    plasma  %>% 
-        filter(pct > 1) %>% 
-        prepare() %>%
-        ungroup() %>% 
-        filter(iso  %in% c("shift5p", "shift3p", "snp")) %>% 
-        mutate(tool = "human plasma",
-               protocol = ifelse(protocol == "TrueSeq", "tru", "neb"))
-) %>% filter(protocol=="tru") %>% 
-    ggplot(aes(pct_total, color = tool)) +
-    geom_density(stat = "ecdf") +
-    facet_grid(pct_cat~iso, scales = "free_x") +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
-          strip.text = element_text(size = 14, color = "black")) +
-    ylab("PCT") # +
-    #scale_color_manual("IMPORTANCE",
-    #                   values = RColorBrewer::brewer.pal(7, "Dark2")[3:7])
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
+    mutate(libsize=log10(sum(value))) %>% 
+    filter(total>500, iso=="iso_snp") %>% 
+    prepare() %>%
+    filter(!(pct_cat %in% c("<0.1", "0.1-1", "1-5"))) %>% 
+    ggplot(aes(x = paste(study, short),
+               fill = pct_cat, y = n)) +
+    geom_bar(stat = "identity") +
+    facet_wrap(~iso_n, ncol = 1, scales = "free_y") +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
+    scale_alpha_discrete(range = c(0.3,1)) +
+    ggtitle("isomiRs importance by size") +
+    ylab("% of sequences") 
+#    ggsave("results/04_other_data/04_other_data_pct_g5.pdf", height = 7)
+
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
+    mutate(libsize=log10(sum(value))) %>% 
+    filter(total>500, iso=="iso_add3p") %>% 
+    prepare() %>%
+    ggplot(aes(x = paste(study, short),
+               fill = pct_cat, y = n,
+               alpha=libsize>5)) +
+    geom_bar(stat = "identity") +
+    facet_wrap(~iso_n, ncol = 1, scales = "free_y") +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
+    scale_alpha_discrete(range = c(0.3,1)) +
+    ggtitle("isomiRs importance by size") +
+    ylab("% of sequences")
+
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
+    mutate(libsize=log10(sum(value))) %>% 
+    filter(total>500, iso=="iso_add5p") %>% 
+    prepare() %>%
+    ggplot(aes(x = paste(study, short),
+               fill = pct_cat, y = n,
+               alpha=libsize>5)) +
+    geom_bar(stat = "identity") +
+    facet_wrap(~iso_n, ncol = 1, scales = "free_y") +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
+    scale_alpha_discrete(range = c(0.3,1)) +
+    ggtitle("isomiRs importance by size") +
+    ylab("% of sequences")
