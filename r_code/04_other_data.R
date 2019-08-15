a1869c3aa573dea128c254a909f7d3a2d50bdcda
Lorena Pantano <lorena.pantano@gmail.com>
2019-08-15 12:22:04 -0400

    move legacy results and start new batch of figures

--- r_code/04_other_data.R
+++ r_code/04_other_data.R
@@ -27,8 +27,93 @@ synthetic %>%
     scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
     scale_alpha_discrete(range = c(0.3,1)) +
     ggtitle("isomiRs importance by type") +
-    ylab("% of sequences") 
+    ylab("% of sequences") +
+    ggsave("results/02_importance/all_samples_barplot.pdf", width=12, height = 9)
 
+
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
+    mutate(libsize=log10(sum(value)),
+           libsizen=n(),
+           iso=relevel(as.factor(iso), "reference")) %>% 
+    filter(total>500) %>% 
+    filter(ref_is_1 == 1) %>%
+    group_by(short, protocol, study, libsize, iso, pct_cat) %>% 
+    summarise(pct_total=sum(value)/unique(10^libsize)*100,
+              pctn_total=n()/unique(libsizen)*100) %>% 
+    ungroup() %>% 
+    ggplot(aes(x = paste(study, short),
+               y = pct_total,
+               alpha=pctn_total,
+               fill=pct_cat)) +
+    geom_bar(stat="identity") +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
+    facet_wrap(~iso, ncol = 1, scales = "free_y") +
+    scale_alpha_continuous(range = c(0.5,1)) +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    ggsave("results/02_importance/all_samples_barplot_abundance.pdf", width=12, height = 9)
+
+
+synthetic %>% 
+    group_by(short, study, protocol) %>%
+    mutate(libsize=log10(sum(value)),
+           libsizen=n(),
+           libsizem=length(unique(mi_rna)),
+           iso=relevel(as.factor(iso), "reference")) %>% 
+    filter(total>500) %>% 
+    filter(ref_is_1 == 1) %>%
+    group_by(short, protocol, study, libsize, mi_rna, iso) %>% 
+    arrange(short, protocol, study, libsize, mi_rna, iso, desc(pct)) %>% 
+    top_n(1, wt = pct) %>% 
+    group_by(short, protocol, study, libsize, iso, pct_cat) %>% 
+    summarise(pct_total=sum(value)/unique(10^libsize)*100,
+              mirn_total=length(unique(mi_rna))) %>% 
+    ungroup() %>% 
+    mutate(protocol=ifelse(protocol=="tru","ill",protocol),
+           protocol=ifelse(protocol=="ilmn","ill",protocol),
+           protocol=ifelse(grepl("nextf", short),"nex",protocol)) %>% 
+    ggplot(aes(x = paste(study, short),
+               y = mirn_total,
+               alpha=pct_total,
+               fill=pct_cat)) +
+    geom_bar(stat="identity") +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")) +
+    facet_wrap(~iso, ncol = 1) +
+    scale_alpha_continuous(range = c(0.5,1)) +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    ggsave("results/02_importance/all_samples_barplot_num_mirna.pdf", width=12, height = 9)    
+
+synthetic %>% 
+    group_by(short, study, protocol) %>% 
+    mutate(libsize=log10(sum(value)),
+           libsizen=n(),
+           libsizem=length(unique(mi_rna)),
+           iso=relevel(as.factor(iso), "reference")) %>% 
+    filter(total>500, pct>5) %>% 
+    filter(ref_is_1 == 1) %>%
+    group_by(short, protocol, study, libsize, mi_rna, iso) %>% 
+    arrange(short, protocol, study, libsize, mi_rna, iso, desc(pct)) %>% 
+    top_n(1,wt = pct) %>% 
+    group_by(short, protocol, study, libsize, iso, pct_cat) %>% 
+    summarise(pct_total=sum(value)/unique(10^libsize)*100,
+              mirn_total=length(unique(mi_rna))) %>% 
+    ungroup() %>% 
+    mutate(protocol=ifelse(protocol=="tru","ill",protocol),
+           protocol=ifelse(protocol=="ilmn","ill",protocol),
+           protocol=ifelse(grepl("nextf", short),"nex",protocol)) %>% 
+    ggplot(aes(x = paste(study, short),
+               y = mirn_total,
+               alpha=pct_total,
+               fill=pct_cat)) +
+    geom_bar(stat="identity") +
+    scale_fill_manual("IMPORTANCE", values = RColorBrewer::brewer.pal(7, "Dark2")[4:7]) +
+    facet_wrap(~iso, ncol = 1) +
+    scale_alpha_continuous(range = c(0.5,1)) +
+    theme_clean() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    ggsave("results/02_importance/all_samples_barplot_num_mirna_5pct.pdf", width=12, height = 9)
 
 prepare =  . %>% filter(ref_is_1 == 1) %>%
     dplyr::count(short, pct_cat, iso, protocol, study, libsize, iso_n) %>% 
